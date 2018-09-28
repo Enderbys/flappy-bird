07f6f0ab812489b6bd6f8f3fcaab7693935ff11b
Enderbys <43681571+Enderbys@users.noreply.github.com>
2018-09-28 20:50:10 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var bird;
+var pipes = [];
+function setup(){
+	createCanvas(400,600);
+	bird = new Bird();
+	pipes.push(new Pipe());
+}
+
+function draw(){
+	background(0);
+	for( var i=0;i<pipes.length;i++){
+		pipes[i].show();
+		pipes[i].update();
+		
+		if(pipes[i].hits(bird)) {
+			console.log("HIT");
+		}
+	}
+	for(var i=pipes.length-1;i>=0;i--)
+	{
+		if(pipes[i].offscreen()){
+			pipes.splice(i,1);
+		}
+	}
+	
+	
+	
+	bird.update();
+	bird.show();
+	
+	if(frameCount % 50 ==0){
+		pipes.push(new Pipe());
+	}
+
+
+}
+
+function keyPressed() {
+	if(key == ' ') {
+		bird.up();
+		console.log("Up");
+	}
+}
